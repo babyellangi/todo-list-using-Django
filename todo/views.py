5d065e16a2cd78d18d7ed5071f435f4ff00e9edf
programming <e.babyle206@gmail.com>
2024-03-11 21:25:14 +0530

    add files

---
+++ todo/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from .forms import todoform
+from . models import todo
+
+# Create your views here.
+def index(request):
+    item_list=todo.objects.order_by('date')
+    if request.method=='POST':
+        form=todoform(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('todo')
+    form=todoform()
+    page={'forms':form,'list':item_list,'title':'TODO LIST'}
+    return render(request,'todo/index.html',page)
+
+def remove(request,item_id):
+    item=todo.objects.get(id=item_id)
+    item.delete()
+    messages.info(request,'item removed !!!')
+    return redirect('todo')
